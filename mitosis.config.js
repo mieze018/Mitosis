0c7c318a96d23e389c9792b8976b841fa536b3d7
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-29 01:17:43 +0900

    🧰  アウトプット設定をVue3にした

--- mitosis.config.js
+++ mitosis.config.js
@@ -1,14 +1,14 @@
-const seedrandom = require('seedrandom');
-const rng = seedrandom('vue-sdk-seed');
+const seedrandom = require("seedrandom");
+const rng = seedrandom("vue-sdk-seed");
 
 const getSeededId = () => {
   const rngVal = rng();
-  return Number(String(rngVal).split('.')[1]).toString(36);
+  return Number(String(rngVal).split(".")[1]).toString(36);
 };
 
 module.exports = {
-  files: 'src/**',
-  targets: ['reactNative', 'vue'],
+  files: "src/**",
+  targets: ["vue3"],
   options: {
     vue: {
       cssNamespace: getSeededId,
